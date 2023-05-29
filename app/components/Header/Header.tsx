61efc2c1ef3bd6fa148ca5656cca8c7737f97f1a
Huy Nguyen <quanghuy.nguyennhu@gmail.com>
2023-05-29 14:13:30 +0300

    intial commit with header & categories bar

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import Categories from "./Categories";
+import Logo from "./Logo";
+import Container from "../Container";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
+interface NavbarProps {
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+}) => {
+  return ( 
+    <div className="
+        fixed
+        w-full
+        bg-white
+        z-10
+        shadow-sm">
+      <div
+        className="
+          py-4
+          border-b-[1px]
+        "
+      >
+      <Container>
+        <div
+          className="
+            flex
+            flex-row
+            items-center
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu />
+        </div>
+      </Container>
+    </div>
+    <Categories />
+  </div>
+  );
+}
+
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    
     extend: {
+      colors:{
+        'mokki-green': '#028446',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
